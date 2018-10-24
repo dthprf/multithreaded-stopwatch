4ced681ea1473fe8377826f4cd9494ba41eafc8d
Bart <bartpetka@gmail.com>
2018-10-24 23:38:41 +0200

    Implement view class

---
+++ stopwatch/src/main/java/stopwatch/View/StopWatchView.java
@@ -0,0 +1,9 @@
+package stopwatch.View;
+
+public class StopWatchView {
+    private static final String COMMAND_REQUEST_MESSAGE = "Enter command:";
+
+    public void displayRequest() {
+        System.out.println(COMMAND_REQUEST_MESSAGE);
+    }
+}
