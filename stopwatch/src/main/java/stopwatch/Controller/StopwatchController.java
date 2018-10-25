a809897de75999bd2298d363afd3a1e54ddaa441
Bart <bartpetka@gmail.com>
2018-10-25 20:44:18 +0200

    Implement runApp method

--- stopwatch/src/main/java/stopwatch/Controller/StopwatchController.java
+++ stopwatch/src/main/java/stopwatch/Controller/StopwatchController.java
@@ -22,5 +22,38 @@ public class StopwatchController {
         this.view = view;
     }
 
+    public void runApp() {
+        boolean isRunning = true;
+
+        while (isRunning) {
+            UserCommand command = parseCommandFromInput(Utils.getInput(
+                    ConsoleMessage.REQUEST_COMMAND.message()));
+
+            switch (command.getCommand()) {
+                case START_COMMAND:
+                    handleStartCommand(command.getStopwatchName());
+                    break;
+
+                case STOP_COMMAND:
+                    handleStopCommand(command.getStopwatchName());
+                    break;
+
+                case CHECK_COMMAND:
+                    handleCheckCommand(command.getStopwatchName());
+                    break;
+
+                case EXIT_COMMAND:
+                    handleExitCommand();
+                    isRunning = false;
+                    break;
+
+                default:
+                    view.displayMessage(ConsoleMessage.COMMAND_ERROR);
+            }
+        }
+    }
+
+
+
 
 }
